203f0386c5e4e51e2350cf625f530325f079a39d
kirill <marteck7@gmail.com>
2023-12-24 20:54:51 +0300

    описание DishDto для свагер

--- src/main/java/ru/fkjob/delivery/rest/dto/DishDto.java
+++ src/main/java/ru/fkjob/delivery/rest/dto/DishDto.java
@@ -1,16 +1,22 @@
 package ru.fkjob.delivery.rest.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel(description = "Блюдо")
 public class DishDto {
 
+    @ApiModelProperty(name = "Идентификатор блюда")
     private Long id;
 
+    @ApiModelProperty(name = "Название блюда")
     private String name;
 
+    @ApiModelProperty(name = "Стоимость блюда")
     private Double price;
 }
